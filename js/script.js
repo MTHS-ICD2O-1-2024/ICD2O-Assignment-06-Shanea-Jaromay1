57e8f12ea5696c15311aba8d7684fa48e2a60a62
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-06-03 17:30:25 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function gets the weather today.
+ * The "async" is there because it will take time for the internet to return the value
+ */
+async function getCoffee() {
+  // the "try" is here because the internet may not be working
+  // it is like an "if ... else" statement"
+  try {
+    const resultJSON = await fetch("https://coffee.alexflipnote.dev/random.json")
+    const jsonData = await resultJSON.json()
+    console.log(jsonData)
+    const imageSrc = jsonData.random.json[0].imageSrc
+
+    // output
+    document.getElementById("coffee-pics").innerHTML = tempCelsius.toFixed(1) + " °C"
+  } catch (error) {
+    console.error(error)
+  }
+}
